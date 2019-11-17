9c7d86a78789c067dc7a7e5c9de768f9406fa269
David <47803353+Norinis@users.noreply.github.com>
2019-11-17 14:01:34 +1100

    New Temp Character and Shader Stuff

---
+++ Assets/Test/Scripts/RotateSkybox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSkybox : MonoBehaviour
+{
+    // Speed multiplier
+    public float speedMultiplier;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Sets the float value of "_Rotation", adjust it by Time.time and a multiplier.
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * speedMultiplier);
+    }
+}
