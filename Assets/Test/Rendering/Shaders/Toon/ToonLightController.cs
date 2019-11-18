5ec53ae30acfce2d37f583ceb53ce05fc0e1fb06
David <47803353+Norinis@users.noreply.github.com>
2019-11-19 01:54:34 +1100

    Sword And Shield

---
+++ Assets/Test/Rendering/Shaders/Toon/ToonLightController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class ToonLightController : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Store the rotation of the light in_Light_Direction
+        Shader.SetGlobalVector("_Light_Direction", -transform.forward);
+    }
+}
+ 
